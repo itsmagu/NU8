9b504b483ba2f032a95d6d84e194419be6277798
magu <95685391+itsmagu@users.noreply.github.com>
2025-04-11 11:07:15 +0200

    Played around with zigs basic memory management

--- build.zig
+++ build.zig
@@ -1,6 +1,11 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const exe = b.addExecutable(.{ .name = "UTF8", .root_source_file = b.path("zig-src/main.zig"), .target = b.graph.host });
+    const exe = b.addExecutable(.{
+         .name = "NU8",
+         .root_source_file = b.path("zig-src/main.zig"),
+         .target = b.graph.host
+    });
+    exe.linkLibC();
     b.installArtifact(exe);
 }
--- zig-src/main.zig
+++ zig-src/main.zig
@@ -1,5 +1,46 @@
 const std = @import("std");
+const print = std.debug.print;
+const mem = std.mem;
 
-pub fn main() void {
-    std.debug.print("Hellow World\n", .{});
+pub fn main() !void {
+    // Open
+    var file = try std.fs.cwd().createFile("output", .{});
+    defer file.close(); // Close file on scope exit
+
+    // Generate
+    var arena = std.heap.ArenaAllocator.init(std.heap.raw_c_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+    const str = "Hellow Guys";
+    print("\nstr is \"{s}\"\n",.{str});
+    
+    print("Arena is {} in size\n", .{arena.queryCapacity()});
+    const memory8B = try allocator.alloc(u8, str.len);
+    print("Arena is {} in size\n", .{arena.queryCapacity()});
+
+    print("Memory is of size {}\n", .{str.len});
+    @memcpy(memory8B, str);
+
+    print("{s}\n",.{memory8B});
+    for (0..memory8B.len-1) |i| {
+        print("{c} - {b}\n", .{memory8B[i],memory8B[i]});
+    }
+
+    // Transformat
+    const memory7B = try allocator.alloc(u7, str.len); // size of u7
+    print("Arena is {} in size\n", .{arena.queryCapacity()});
+    for (0..memory7B.len-1) |i| {
+        print("{c} - {b}\n", .{memory7B[i],memory7B[i]});
+    }
+
+    // Write
+    try file.writeAll(memory8B);
 }
+
+fn b8ToB7(in: []u8) []u7 {
+    _ = in;
+}
+
+// fn b7ToB8(in: []const u7) []const u8 {
+    
+// }
