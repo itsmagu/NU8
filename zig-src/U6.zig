574875e242e94df50e7c06cfd049495f7a48c375
magu <95685391+itsmagu@users.noreply.github.com>
2025-04-16 18:44:03 +0200

    U6 File and Interactive
    Moved the U6 format to its own file and made the program interactable

---
+++ zig-src/U6.zig
@@ -0,0 +1,108 @@
+const std = @import("std");
+const print = std.debug.print;
+
+// Our 24 bit type
+pub const Set = packed struct { pri: u6, duo: u6, tri: u6, tet: u6 };
+
+pub fn charTob6(in: u8) u6 {
+    return switch (in) {
+        48...57 => @intCast(in + 5),
+        65...90 => @intCast(in - 65),
+        97...122 => @intCast((in - 71)),
+        32 => 62,
+        10 => 63,
+        else => 23,
+    };
+}
+
+pub fn b6ToChar(in: u6) u8 {
+    return switch (in) {
+        0...25 => @as(u8, in) + 65,
+        26...51 => @as(u8, in) + 71,
+        52...61 => @as(u8, in) - 4,
+        62 => 32,
+        63 => 10,
+    };
+}
+
+pub fn b8ToB6(allocator: std.mem.Allocator, src: []u8) []Set {
+    const sets = src.len / 4;
+    const reminder = src.len % 4;
+    var dest: []Set = undefined;
+    if (reminder != 0) {
+        dest = allocator.alloc(Set, sets+1) catch &[_]Set{};
+    } else {
+        dest = allocator.alloc(Set, sets) catch &[_]Set{};
+    }
+    for (0..sets) |set| {
+        const i = set * 4;
+        dest[set].pri = charTob6(src[i]);
+        dest[set].duo = charTob6(src[i + 1]);
+        dest[set].tri = charTob6(src[i + 2]);
+        dest[set].tet = charTob6(src[i + 3]);
+    }
+    switch (reminder) {
+        1 => {
+            dest[sets].pri = charTob6(src[sets * 4]);
+            dest[sets].duo = 62;
+            dest[sets].tri = 62;
+            dest[sets].tet = 62;
+        },
+        2 => {
+            dest[sets].pri = charTob6(src[sets * 4]);
+            dest[sets].duo = charTob6(src[sets * 4 + 1]);
+            dest[sets].tri = 62;
+            dest[sets].tet = 62;
+        },
+        3 => {
+            dest[sets].pri = charTob6(src[sets * 4]);
+            dest[sets].duo = charTob6(src[sets * 4 + 1]);
+            dest[sets].tri = charTob6(src[sets * 4 + 2]);
+            dest[sets].tet = 62;
+        },
+        else => {},
+    }
+    return dest;
+}
+
+pub fn b6ToB8(allocator: std.mem.Allocator, src: []Set) []u8 {
+    var dest = allocator.alloc(u8, src.len*4) catch return "";
+    for (src,0..) |set,i| {
+        dest[i*4] = b6ToChar(set.pri);
+        dest[i*4+1] = b6ToChar(set.duo);
+        dest[i*4+2] = b6ToChar(set.tri);
+        dest[i*4+3] = b6ToChar(set.tet);
+    }
+    return dest;
+    //TODO strip trailing spaces
+}
+
+pub fn printB6(src: []Set) void { // Debug
+    const stdin = std.io.getStdOut().writer();
+    for (0..src.len) |i| {
+        stdin.print("{b} {b} {b} {b} = {c} {c} {c} {c}\n", .{ src[i].pri, src[i].duo, src[i].tri, src[i].tet, b6ToChar(src[i].pri), b6ToChar(src[i].duo), b6ToChar(src[i].tri), b6ToChar(src[i].tet) }) catch unreachable;
+    }
+}
+
+pub fn loadB6FromFile(file: std.fs.File, allocator: std.mem.Allocator) ![]Set {
+    const stat = try file.stat();
+    // print("{} bits in {} bytes\n", .{ stat.size * 8, stat.size });
+    const memory = try allocator.alloc(Set, stat.size / 3);
+    // print("We will read {} bytes into {} sets\n", .{ stat.size, stat.size / 3 });
+    var reader = std.io.bitReader(.big, file.reader());
+    for (memory, 0..) |_, i| {
+        const bits = try reader.readBitsNoEof(u24, 24);
+        // print("{b}\n", .{bits});
+        const sets: *Set = @ptrCast(@constCast(&bits));
+        memory[i] = sets.*;
+    }
+    return memory;
+}
+
+pub fn writeB6ToFile(memory6B: []Set, file: std.fs.File) !void {
+    const memoryptr: *[]u24 = @ptrCast(@constCast(&memory6B));
+    var writer = std.io.bitWriter(.big, file.writer());
+    for (memory6B, 0..) |_, i| {
+        try writer.writeBits(memoryptr.*[i], 24);
+    }
+}
--- zig-src/main.zig
+++ zig-src/main.zig
@@ -1,123 +1,87 @@
+// I used this project to learn ZIG
 const std = @import("std");
-const print = std.debug.print;
-const mem = std.mem;
+const U6 = @import("U6.zig");
+const Allocator = std.mem.Allocator; // Import the Allocator Type
 
-// Let main look as close to client/user code as possible!
 pub fn main() !void {
-    // Open
-    var file = try std.fs.cwd().createFile("output", .{});
-
-    // Generate
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
+    defer arena.deinit(); // This is why we don't need garbage collection B)
     const allocator = arena.allocator();
-    const str = "This is cool";
-    print("\nstr is \"{s}\"\n", .{str});
-
-    const memory8B = try allocator.alloc(u8, str.len);
-    print("Memory8B is of size {}\n", .{memory8B.len * @bitSizeOf(u8)});
-    @memcpy(memory8B, str);
-
-    // Transformat
-    var sets = str.len / 4;
-    if (str.len % 4 != 0) {
-        sets += 1;
-    }
-    const memory6B = try allocator.alloc(Set, sets);
-    print("Memory6B is of size {}\n", .{memory6B.len * @bitSizeOf(Set)});
-    b8ToB6(memory8B, memory6B);
-
-    // Print
-    printB6(memory6B);
-
-    // Write file
-    const memoryptr: *[]u24 = @ptrCast(@constCast(&memory6B));
-    var writer = std.io.bitWriter(.little, file.writer());
-    for (memory6B, 0..) |_, i| {
-        try writer.writeBits(memoryptr.*[i], 24);
-    }
-    file.close();
-    // Read file
-    const readfile = try std.fs.cwd().openFile("output", .{});
-    const newmem6B = try loadB6fromFile(readfile, allocator);
-    printB6(newmem6B);
-    readfile.close();
-}
-
-// Our 24 bit type
-const Set = packed struct { pri: u6, duo: u6, tri: u6, tet: u6 };
-
-fn charTob6(in: u8) u6 {
-    return switch (in) {
-        48...57 => @intCast(in + 5),
-        65...90 => @intCast(in - 65),
-        97...122 => @intCast((in - 71)),
-        32 => 62,
-        10 => 63,
-        else => 23,
-    };
-}
-
-fn b6ToChar(in: u6) u8 {
-    return switch (in) {
-        0...25 => @as(u8, in) + 65,
-        26...51 => @as(u8, in) + 71,
-        52...61 => @as(u8, in) - 4,
-        62 => 32,
-        63 => 10,
-    };
-}
-
-fn b8ToB6(src: []u8, dest: []Set) void {
-    const sets = src.len / 4;
-    const reminder = src.len % 4;
-    for (0..sets) |set| {
-        const i = set * 4;
-        dest[set].pri = charTob6(src[i]);
-        dest[set].duo = charTob6(src[i + 1]);
-        dest[set].tri = charTob6(src[i + 2]);
-        dest[set].tet = charTob6(src[i + 3]);
-    }
-    switch (reminder) {
-        1 => {
-            dest[sets].pri = charTob6(src[sets * 4]);
-            dest[sets].duo = 62;
-            dest[sets].tri = 62;
-            dest[sets].tet = 62;
-        },
-        2 => {
-            dest[sets].pri = charTob6(src[sets * 4]);
-            dest[sets].duo = charTob6(src[sets * 4 + 1]);
-            dest[sets].tri = 62;
-            dest[sets].tet = 62;
-        },
-        3 => {
-            dest[sets].pri = charTob6(src[sets * 4]);
-            dest[sets].duo = charTob6(src[sets * 4 + 1]);
-            dest[sets].tri = charTob6(src[sets * 4 + 2]);
-            dest[sets].tet = 62;
-        },
-        else => {},
+    print("Do you want to read a file[\"R\"] or write a line[\"W\"]");
+    const in = try input(allocator);
+    if (std.mem.eql(u8, in, "W")) {
+        print("-" ** 20); // Very Python, I like it!
+        print("The U6 format can only store a-z, A-Z, 0-9 and space"); // We can't include newline from a console read :(
+        print("Write the text you want to store? MAX 128 Symbols!");
+        const toWrite = try input(allocator);
+        print("-" ** 20); // Very Python, I like it!
+        print("What should be the name of the file? It will be stored in the current work directory");
+        const fileName = try input(allocator);
+        const mem = U6.b8ToB6(allocator, @as([]u8, @constCast(toWrite)));
+        const file = try std.fs.cwd().createFile(fileName, .{});
+        try U6.writeB6ToFile(mem, file);
+        file.close();
+    } else if (std.mem.eql(u8, in, "R")) {
+        print("What is the filename we should read in the current work directory?");
+        const fileName = try input(allocator);
+        if (std.fs.cwd().openFile(fileName, .{})) |file| {
+            print("-" ** 20); // Very Python, I like it!
+            const mem6B = try U6.loadB6FromFile(file, allocator);
+            U6.printB6(mem6B);
+            print(U6.b6ToB8(allocator, mem6B));
+            file.close();
+        } else |err| switch (err) {
+            error.FileNotFound => {
+                print("File was not found in the current work directory!");
+            },
+            error.InvalidUtf8 => {
+                print("Filename is not valid UTF8");
+            },
+            error.FileTooBig => {
+                print("File was too big");
+            },
+            error.DeviceBusy => {
+                print("Device is busy Error!");
+            },
+            error.AccessDenied => {
+                print("Access Denied Error!");
+            },
+            error.SystemResources => {
+                print("System Resrouces Error!");
+            },
+            error.NoDevice => {
+                print("No Device Error!");
+            },
+            error.AntivirusInterference => {
+                print("Antivirus Interferance Error!");
+            },
+            error.SymLinkLoop => {
+                print("Symlink loop Error!");
+            },
+            error.IsDir => {
+                print("Filename is a Directory Error!");
+            },
+            error.FileBusy => {
+                print("File is busy Error!");
+            },
+            else => {},
+        }
+    } else {
+        print("-" ** 20); // Very Python, I like it!
+        print("Not a valid operation!\nWrite a \"R\" or \"W\" next time");
     }
 }
 
-fn printB6(src: []Set) void {
-    for (0..src.len) |i| {
-        print("{b} {b} {b} {b} = {c} {c} {c} {c}\n", .{ src[i].pri, src[i].duo, src[i].tri, src[i].tet, b6ToChar(src[i].pri), b6ToChar(src[i].duo), b6ToChar(src[i].tri), b6ToChar(src[i].tet) });
-    }
+fn print(text: []const u8) void {
+    const stdout = std.io.getStdOut().writer(); // We could cache this but whatever
+    stdout.print("{s}\n", .{text}) catch unreachable; // Catch unreachable throws away erros, so we don't need to try on every print
 }
 
-fn loadB6fromFile(file: std.fs.File, allocator: std.mem.Allocator) ![]Set {
-    const stat = try file.stat();
-    print("{} bits in {} bytes\n", .{stat.size * 8,stat.size});
-    const memory = try allocator.alloc(Set, stat.size / 3);
-    print("We will read {} bytes into {} sets\n", .{stat.size,stat.size/3});
-    var reader = std.io.bitReader(.little, file.reader());
-    for (memory, 0..) |_, i| {
-        const bits = try reader.readBitsNoEof(u24, 24);
-        print("{b}\n", .{bits});
-        const sets: *Set = @ptrCast(@constCast(&bits));
-        memory[i] = sets.*;
-    }
-    return memory;
+fn input(allocator: Allocator) ![]const u8 {
+    const stdin = std.io.getStdIn().reader();
+    // The buffer is only 128 bytes so don't get your hopes up
+    const bufinput: []u8 = try stdin.readUntilDelimiterOrEofAlloc(allocator, '\n', 128) orelse @constCast("");
+    // We don't have to free this buffer since the passed allocator is a arena
+    const result = std.mem.trimRight(u8, bufinput[0 .. bufinput.len - 1], "\r");
+    return result;
 }
